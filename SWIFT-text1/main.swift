887fe597f4ffb86243c477fb4b410599eb344ecd
ciecS20171106331 <565738655@qq.com>
2018-09-29 10:01:31 +0800

    累加
    累加

--- SWIFT-text1/main.swift
+++ SWIFT-text1/main.swift
@@ -8,5 +8,22 @@
 
 import Foundation
 
-print("Hello, World!")
 
+
+var c:[Int]=[1,4,3,2,6,5,9]
+for i in 0..<c.count
+{
+    for j in 1..<c.count-i
+    {
+        if(c[j]>c[j-1])
+        {
+            let t=c[j]
+            c[j]=c[j-1]
+            c[j-1]=t
+        }
+    }
+}
+for x in 0..<c.count
+{
+    print(c[x])
+}
